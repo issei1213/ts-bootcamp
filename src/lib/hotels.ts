8e055ba0d9ae62f8302ad15ca4648ab2e2ec515c
issei1213 <lord_0bf38w16110335h@yahoo.co.jp>
2021-01-16 09:39:21 +0900

    updating hotels.ts

--- src/lib/hotels.ts
+++ src/lib/hotels.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
 import { APP_ID, BASE_URL } from "rakuten/config";
-import { Hotels } from "types/hotels"
+import { Hotels } from "types/hotels";
 
-export const searchHotels = async(keyword: string): Promise<Hotels> => {
-  try{
-    const res = await axios.get(BASE_URL + "&applicationId=" + APP_ID + "&keyword=" + keyword );
-    console.log('res.data.hotels', res.data.hotels)
-    return res.data.hotels
-
-  } catch(e) {
-    console.error(e)
-    return[]
+interface SearchHotelData {
+  hotels: Hotels,
+  pagingInfo: {
+    first: number
+    last: number
+    page: number
+    pageCount: number
+    recordCount: number
   }
-
 }
+
+export const searchHotels = async (keyword: string): Promise<Hotels> => {
+  try {
+    const res = await axios.get<SearchHotelData>(BASE_URL + "&applicationId=" + APP_ID + "&keyword=" + keyword);
+    console.log("res.data.hotels", res.data.hotels);
+    return res.data.hotels;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
